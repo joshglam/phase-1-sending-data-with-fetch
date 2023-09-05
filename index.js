c6c8e29f24f0d05d1563ffe1dfb20b5772b2122b
josh <jglam88@gmail.com>
2023-09-05 13:56:00 -0400

    completes fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+// const userName = "Josh"
+// const userEmail = "test@gmail.com"
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({
+        name: userName,
+        email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(user => {
+        const append = document.createElement("p")
+        const body = document.querySelector("#body")
+        append.textContent = user.id
+        body.append(append)
+
+    })
+    .catch(function (error) {
+        const append = document.createElement("p")
+        const body = document.querySelector("#body")
+        append.textContent = error.message
+        body.append(append)
+    })
+}
